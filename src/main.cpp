53434514631ec28df9cdda2de81e11156c882a67
tsingfa <tsingfa6@163.com>
2024-06-02 11:58:59 +0800

    实现二维向量的距离和旋转

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "../include/PVZ/CoreMinimal.h"
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+    Vector2D A(-1, -1);
+    Vector2D B(1, 1);
+    cout << Vector2D::Distance(A, B) << endl;
+
+    Vector2D C(1, 0);
+    cout << Vector2D::RotateVector(C, 90) << endl;
+
+    return 0;
+}
